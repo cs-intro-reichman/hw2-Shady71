b78a3b8136d26c8ab53c8ad4b9993461abcb4d73
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-11-21 14:37:55 +0200

    made pi calculator

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,24 @@
+import javax.swing.*;
+
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String [] args) {
+
+		int numTerms = Integer.parseInt(args[0]);
+		int n = 0;
+		double sum = 0;
+
+		for(int i = 0; i < numTerms; i++) {
+			double formula = Math.pow(-1, n) / ((2 * n) + 1);
+			sum += formula;
+			n++;
+		}
+
+		double javaPi = Math.PI;
+		System.out.println("pi according to java: " + javaPi);
+
+		double finalAnswer = 4 * sum;
+		System.out.println("pi approximated: " + finalAnswer);
+
 	}
 }
