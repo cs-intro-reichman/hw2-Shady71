4d97e79fb284668612de3463d31720fe54946555
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-11-21 14:38:48 +0200

    wrote time calc and test random

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,16 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minToAdd = Integer.parseInt(args[1]);
+
+        int totalMins = (hours * 60) + minutes + minToAdd;
+        int totalHours = totalMins / 60;
+        int newHour = totalHours % 24;
+        int newMin = totalMins - (totalHours * 60);
+
+
+
+        System.out.println(newHour + ":" + newMin);
     }
 }
